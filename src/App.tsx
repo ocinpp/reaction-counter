0d899727f7811a96af6ff8c24a4ca968415c14cf
ocinpp <ocin25@gmail.com>
2025-04-06 21:26:43 +0800

    Add GPU delegate

--- src/App.tsx
+++ src/App.tsx
@@ -59,7 +59,9 @@ export default function Home() {
           {
             baseOptions: {
               modelAssetPath:
-                "https://storage.googleapis.com/mediapipe-models/gesture_recognizer/gesture_recognizer/float16/1/gesture_recognizer.task",
+                "https://storage.googleapis.com/mediapipe-models/gesture_recognizer/gesture_recognizer/float16/1/gesture_recognizer.task",,
+              delegate:
+                "GPU", // Use GPU delegate for better performance, fallback to CPU if not supported
             },
             runningMode: "VIDEO",
             numHands: 1,
